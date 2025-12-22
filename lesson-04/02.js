7b0a34fc40133535d4f28496f48e5ea172e311d2
Tony Koshev <anton123koshev@gmail.com>
2025-12-22 18:02:42 +0300

    lesson-04-done

---
+++ lesson-04/02.js
@@ -0,0 +1,41 @@
+/*
+Напишите функцию `findUniqueElements`, которая принимает массив и возвращает новый массив, содержащий только уникальные элементы из исходного массива.
+
+**Входные данные:**
+
+- **`array`**: Массив, который может содержать повторяющиеся элементы.
+
+**Выходные данные:**
+
+- Массив, содержащий только уникальные элементы из исходного массива.
+
+**Пример использования:**
+`findUniqueElements([1, 2, 3, 2, 1, 4])` вернёт `[1, 2, 3, 4]`.
+
+Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
+*/
+
+function includesElement(array, element) {
+    let trueOrFalse = false;
+    for (let i = 0; i < array.length; i++) {
+        const arrayElement = array[i];
+        if (element === arrayElement) {
+            trueOrFalse = true
+        }
+        
+    }
+    return trueOrFalse
+}
+
+function findUniqueElements(array) {
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        const arrayElement = array[i];
+        const probega2 = includesElement(newArray, arrayElement)
+        if(!probega2) {
+            newArray.push(arrayElement)
+                
+            }
+            }
+    return newArray
+}
---
+++ lesson-04/03.js
@@ -0,0 +1,44 @@
+/*
+Напишите функцию `findCommonElements`, которая принимает два массива и возвращает массив, содержащий общие элементы из обоих массивов.
+
+Входные данные:
+
+- `array1`: Первый массив, содержащий любые типы данных.
+- `array2`: Второй массив, содержащий любые типы данных.
+
+Выходные данные:
+
+- Массив, содержащий элементы, которые присутствуют в обоих исходных массивах.
+- Если общих элементов нет, должен вернуться пустой массив
+
+Пример использования:
+findCommonElements([1, 2, 3], [2, 3, 4]) // [2, 3]
+
+Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
+*/
+
+function includesElement(array, element) {
+    let trueOrFalse = false;
+    for (let i = 0; i < array.length; i++) {
+        const arrayElement = array[i];
+        if (element === arrayElement) {
+            trueOrFalse = true
+        }
+        
+    }
+    return trueOrFalse
+}
+
+
+function findCommonElements(array1, array2) {
+    const gluedArray = [];
+    for (let i = 0; i < array1.length; i++) {
+        const currentElement = array1[i];
+        if(includesElement(array2, currentElement)){
+            gluedArray.push(currentElement)
+        }
+        
+    }
+    return gluedArray
+}
+console.log(findCommonElements(array1, array2))
